4bb06bfed31a34a4fcfbda4bd0580a7f4dc1ba7a
SofiVne <soufiane.mbarki@efrei.net>
2024-01-08 19:56:16 +0100

    adding some folders : analyzis,utils, models...

--- scala_streaming/src/main/scala/MainApp.scala
+++ scala_streaming/src/main/scala/MainApp.scala
@@ -1,10 +1,25 @@
-import zio._
+import zio.*
+import zio.stream.ZStream
+import com.github.tototoshi.csv.*
+import models.Artist
+import zio.Console.*
 
-object MainApp extends ZIOAppDefault {
-  def run =
+
+
+object SpotifyMergeStreams extends ZIOAppDefault {
+  
+  override val run: ZIO[Any & ZIOAppArgs & Scope, Throwable, Unit] =
     for {
-      _    <- Console.print("Please enter your name: ")
-      name <- Console.readLine
-      _    <- Console.printLine(s"Hello, $name!")
+      artistStream <- createArtistStream("Artists.csv")
+      artist <- artistStream.foreach(artist => Console.printLine(artist.toString))
     } yield ()
+
+  private def createArtistStream(fileName: String): ZIO[Any, Throwable, ZStream[Any, Throwable, Artist]] =
+    for {
+      url <- ZIO.succeed(getClass.getClassLoader.getResource(fileName))
+      source <- ZIO.succeed(CSVReader.open(url.getFile))
+      artists <- ZIO.succeed(source.toStream
+        .map(row => Artist(row.head, row(1), row(2), row(3), row(4), row(5), row(6), row(7), row(8))))
+    } yield ZStream.fromIterable(artists)
+
 }
---
+++ scala_streaming/src/main/scala/models/Artist.scala
@@ -0,0 +1,3 @@
+package models
+
+case class Artist(name: String, ID: String, Gender: String, Age: String, Country: String, Genres: String, Popularity: String, Followers: String, URI: String)
